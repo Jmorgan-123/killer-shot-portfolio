a79b9770235baf8b5f1279fc11afc55a43cab234
Gamer Jzs <165060451+Jmorgan-123@users.noreply.github.com>
2024-04-04 03:04:42 +0300

    main.cpp

--- Area_of_shapes/c++_code/main.cpp
+++ Area_of_shapes/c++_code/main.cpp
@@ -1,62 +1,55 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-// Function to calculate area of a circle
-double areaOfCircle(double radius) {
-    return M_PI * radius * radius;
+double calculateCircleArea(double radius) {
+    return M_PI * pow(radius, 2);
 }
 
-// Function to calculate area of a rectangle
-double areaOfRectangle(double length, double width) {
+double calculateRectangleArea(double length, double width) {
     return length * width;
 }
 
-// Function to calculate area of a triangle
-double areaOfTriangle(double base, double height) {
+double calculateTriangleArea(double base, double height) {
     return 0.5 * base * height;
 }
 
 int main() {
-    char choice;
-    double radius, length, width, base, height;
+    int choice;
+    cout << "Choose a shape:" << endl;
+    cout << "1. Circle" << endl;
+    cout << "2. Rectangle" << endl;
+    cout << "3. Triangle" << endl;
 
-    do {
-        cout << "Choose a shape to calculate its area:\n";
-        cout << "1. Circle\n";
-        cout << "2. Rectangle\n";
-        cout << "3. Triangle\n";
-        cout << "Enter your choice (1-3): ";
-        cin >> choice;
+    cout << "Enter your choice (1/2/3): ";
+    cin >> choice;
 
-        switch(choice) {
-            case '1':
-                cout << "Enter the radius of the circle: ";
-                cin >> radius;
-                cout << "Area of the circle: " << areaOfCircle(radius) << endl;
-                break;
-            case '2':
-                cout << "Enter the length of the rectangle: ";
-                cin >> length;
-                cout << "Enter the width of the rectangle: ";
-                cin >> width;
-                cout << "Area of the rectangle: " << areaOfRectangle(length, width) << endl;
-                break;
-            case '3':
-                cout << "Enter the base of the triangle: ";
-                cin >> base;
-                cout << "Enter the height of the triangle: ";
-                cin >> height;
-                cout << "Area of the triangle: " << areaOfTriangle(base, height) << endl;
-                break;
-            default:
-                cout << "Invalid choice! Please enter a number from 1 to 3.\n";
-        }
-
-        cout << "Do you want to calculate the area of another shape? (Y/N): ";
-        cin >> choice;
-    } while (choice == 'Y' || choice == 'y');
+    if (choice == 1) {
+        double radius;
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        double area = calculateCircleArea(radius);
+        cout << "The area of the circle is: " << area << endl;
+    } else if (choice == 2) {
+        double length, width;
+        cout << "Enter the length of the rectangle: ";
+        cin >> length;
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        double area = calculateRectangleArea(length, width);
+        cout << "The area of the rectangle is: " << area << endl;
+    } else if (choice == 3) {
+        double base, height;
+        cout << "Enter the base of the triangle: ";
+        cin >> base;
+        cout << "Enter the height of the triangle: ";
+        cin >> height;
+        double area = calculateTriangleArea(base, height);
+        cout << "The area of the triangle is: " << area << endl;
+    } else {
+        cout << "Invalid choice." << endl;
+    }
 
     return 0;
 }
+
